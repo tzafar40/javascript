f913cf476f3aa8e731fb6b48c8ddf07858e468ae
tzafar40 <55066452+tzafar40@users.noreply.github.com>
2020-03-02 13:57:30 +1100

    Add files via upload
    Create http server using node and to verify open your web browser and go to 'http://localhost:8080'
    The first line is just getting the http module and saving it to the variable 'http'. The http is included with Node.js to make it easy for us to create Node.js applications.
    Then http server is created  by calling it's function 'createServer'. This function returns an http object and takes  function in parameter.
    Next the function 'listen' on new server object is called which takes in value 8080 and tells server to which port to listen . This will connect browser to http://localhost:8080

---
+++ pattern.js
@@ -0,0 +1,10 @@
+console.log("pattern starts")
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server started');
